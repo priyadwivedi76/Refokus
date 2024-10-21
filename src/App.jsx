fd0688aebc82b2dbdbf9e47b01770ff777068387
Dwivedi Priya <pd7668665@gmail.com>
2024-10-22 03:17:51 +0530

    Work-Section

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 import React from "react"
 import Navbar from "./components/Navbar"
-
+import Work from "./components/Work"
 function App() {
 
   return (
     <>
     <div className="h-screen w-full font-['satoshi_variable'] bg-zinc-900 text-white">
       <Navbar/>
+      <Work/>
     </div>
     </>
   )
