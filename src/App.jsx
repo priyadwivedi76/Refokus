a38db909288aa802694b1a1466bc545c3c4cafe3
Dwivedi Priya <pd7668665@gmail.com>
2024-10-22 19:45:38 +0530

    Products

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,19 @@ import React from "react"
 import Navbar from "./components/Navbar"
 import Work from "./components/Work"
 import Stripes from "./components/Stripes"
+import Products from "./components/Products"
+
+
+
 function App() {
 
   return (
     <>
-    <div className="h-screen w-full font-['satoshi_variable'] bg-zinc-900 text-white">
+    <div className="min-h-screen w-full font-['satoshi_variable'] bg-zinc-900 text-white">
       <Navbar/>
       <Work/>
       <Stripes />
-
+      <Products/>
     </div>
     </>
   )
--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import { FiCornerDownRight } from "react-icons/fi";
 
-const Button=()=>{
+const Button=({title="Get Started"})=>{
     return(
-        <div className="min-w-40 px-4 py-2 bg-zinc-100 rounded-full text-black flex justify-between items-center">
-            <span className="text-sm font-medium">Get Started</span>
+        <div className="w-40 px-4 py-2 bg-zinc-100 rounded-full text-black flex justify-between items-center">
+            <span className="text-sm font-medium">{title}</span>
             <FiCornerDownRight/>
         </div>
     )
